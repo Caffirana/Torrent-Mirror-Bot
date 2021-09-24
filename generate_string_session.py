70c692a48d0667983c81a24fdccb074b9e32d07e
SahanJayaweera <85601150+SahanJayaweera@users.noreply.github.com>
2021-09-24 14:20:35 +0530

    Update generate_string_session.py

--- generate_string_session.py
+++ generate_string_session.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-API_KEY = int(input("Enter API KEY: "))
-API_HASH = input("Enter API HASH: ")
+API_KEY = int(input("6951121"))
+API_HASH = input("10654d2aa0d67e2f8759ba6a1644fa65")
 with Client(':memory:', api_id=API_KEY, api_hash=API_HASH) as app:
     print(app.export_session_string())
